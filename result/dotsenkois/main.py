eec92a74e29b807af1bdaffd84cba9bd5a88c7dd
dotsenkois <dotsenkois@yandex.ru>
2022-08-13 20:45:46 +0700

    fix

--- result/dotsenkois/main.py
+++ result/dotsenkois/main.py
@@ -9,9 +9,9 @@ import re
 import os.path
 
 # файл для записи номера последней успешнообработаной страницы
-log_file = '/home/dotsenkois/log.txt'
+log_file = 'log.txt'
 #Начальная страница для интервала опроса по-умолчанию
-first_page = 201003445
+first_page = 201003445 
 
 # Если существует файл лога, то номер первой страницы вычитывается из него
 if os.path.exists(log_file):
@@ -25,14 +25,14 @@ last_page = 201600000
 # Генерация списка для работы цикла
 pages = [i for i in range(first_page, last_page)]
 # Генерация имени файла для сохраннения результатов
-export_filename = f'/home/dotsenkois/export.csv'
-# Основной домен для перебора адресов
+export_filename = f'export.csv'
+# Основной домен для перебора адресов 
 base_url = "https://kinescope.io/embed/"
 
 # Формируем шаблон словаря для экспорта в csv
 
 export = {
-    'provider': '',
+    'provider': '', 
     'URL': '',
     'course': '',
     'webinar_date': '',
@@ -41,7 +41,7 @@ export = {
     }
 # Проверяем наличе файла для экспорта. Если отсуствует, то создаем
 if not os.path.exists(export_filename):
-    with open(export_filename, 'a', encoding='utf-8') as f:
+    with open(export_filename, 'a', encoding='utf-8') as f:  
         w = csv.DictWriter(f, export.keys(),delimiter='\t')
         w.writeheader()
 
@@ -53,15 +53,15 @@ for page in pages:
     response = requests.get(url)
     # Создаем пустой временный словарь или очищаем от данных при последующих итерациях
     tmp_dict = {}
-    # Ищем в HTML совпадения для того, чтобы исключить обработку страниц
-    retext = response.text.find("<title>Access forbidden</title>")
+    # Ищем в HTML совпадения для того, чтобы исключить обработку страниц 
+    retext = response.text.find("<title>Access forbidden</title>") 
     tennchat = response.text.find("<title>tenchat-user-story</title>")
     # Проверяем статус код и наличие вхождений от поиска: Если статус код 200 и на странце нет вышеуказанного текста, то продолжаем обработку
-    if response.status_code == 200 and retext == -1 and tennchat == -1:
+    if response.status_code == 200 and retext == -1 and tennchat == -1:    
         soup = BeautifulSoup(response.text, 'lxml')
         # ищем тег для обработки. Информация берется только из него
         tag_script = soup.find('script', {'type': 'application/ld+json'})
-        # продолжаем обработку, если tag_script содержит данные
+        # продолжаем обработку, если tag_script содержит данные 
         if tag_script != None:
             # Преобразуем теккстовые данные
             description = str(tag_script.contents[0]).replace('\n','').replace('{','').replace('}','').lstrip()
@@ -77,7 +77,7 @@ for page in pages:
                 #     tmp_dict.setdefault((a[0].rstrip()).lstrip().replace('"',''), (a[0].rstrip()).lstrip().replace('"',''))
 
             # проверяем на соотвествие названия вебинара шаблону
-            m = re.findall(r'^\w*[-]\d\d?', tmp_dict['name'] )
+            m = re.findall(r'\w*[-]\d\d?', tmp_dict['name'] )
             # Значение переменные provider и course по-умолчанию
             provider = 'Other'
             course = 'Other'
@@ -87,7 +87,7 @@ for page in pages:
                 course = m[0]
             # формируем словарь для экспорт в csv
             export = {
-                'provider': provider,
+                'provider': provider, 
                 'URL': url,
                 'course': course,
                 'webinar_date': tmp_dict['uploadDate'],
@@ -95,7 +95,7 @@ for page in pages:
                 'webinare_title': tmp_dict['name'],
                 }
             # Дозаписываем данные в файл
-            with open(export_filename, 'a', encoding='utf-8') as f:
+            with open(export_filename, 'a', encoding='utf-8') as f: 
                 w = csv.DictWriter(f, export.keys(),delimiter='\t')
                 w.writerow(export)
             # Записываем номер последней успешнообработанной страницы
